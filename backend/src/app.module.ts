f71a21c792d4d9555e085dd121e569e2de0b94a3
CreeKeem <388cjk@gmail.com>
2025-06-07 19:02:04 -0500

    clean up some files

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
-import { UserModule } from './user/user.module';
 import { SetModule } from './set/set.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { FlashcardModule } from './flashcard/flashcard.module';
 
 
 @Module({
-  imports: [AuthModule, UserModule, SetModule, FlashcardModule, PrismaModule],
+  imports: [AuthModule, SetModule, FlashcardModule, PrismaModule],
 })
 export class AppModule {}
