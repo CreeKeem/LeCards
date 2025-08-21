f115fd3b6072f577ec4533a23be591fbe401b35d
CreeKeem <388cjk@gmail.com>
2025-08-21 12:58:04 -0500

    added final slide for the flashcards to give options after finishing

--- frontend/src/components/learn/flashcard/flashcard-learn.tsx
+++ frontend/src/components/learn/flashcard/flashcard-learn.tsx
@@ -60,9 +60,15 @@ export const FlashcardLearn = ({
   };
 
   const goToNext = () => {
-    if (index < cards.length - 1) {
+    if (index < cards.length) {
       setIndex(index + 1);
     }
+  };
+
+  const handleShuffle = () => {
+    const shuffledCards = cards.sort(() => Math.random() - 0.5);
+    setCards(shuffledCards);
+    setIndex(0);
   };
 
   if (setId === -1) {
@@ -96,7 +102,10 @@ export const FlashcardLearn = ({
     <div className="w-full max-w-5xl h-[700px] flex flex-col gap-4">
       {/* Back Button and Card Indicator */}
       <div className="flex items-center relative w-full">
-        <span className="text-lg font-medium mx-auto">
+        <span
+          className={`text-lg font-medium mx-auto
+          ${index >= cards.length && "invisible"}`}
+        >
           {index + 1} of {cards.length}
         </span>
       </div>
@@ -105,9 +114,7 @@ export const FlashcardLearn = ({
       <div className="flex-1 flex items-center justify-center gap-4 min-h-0">
         <button
           className={`flex-shrink-0 h-full max-h-[500px] w-16 rounded-xl flex items-center justify-center transition-all duration-300 ${
-            index > 0
-              ? "hover:bg-gray-300 cursor-pointer"
-              : "opacity-50 cursor-not-allowed"
+            index > 0 ? "hover:bg-gray-300 cursor-pointer" : "invisible"
           }`}
           onClick={goToPrevious}
           disabled={index === 0}
@@ -115,25 +122,68 @@ export const FlashcardLearn = ({
           <ChevronIcon className="h-8 w-8 select-none text-gray-700 rotate-180" />
         </button>
 
-        <div className="flex-1 h-full max-h-[500px]">
-          <FlashcardFlip flashcardDto={cards[index]} />
-        </div>
+        {index < cards.length ? (
+          <div className="flex-1 h-full max-h-[500px]">
+            <FlashcardFlip flashcardDto={cards[index]} />
+          </div>
+        ) : (
+          <div className="flex-1 h-full max-h-[500px]">
+            <div className="bg-white w-full h-full rounded-2xl shadow-lg flex flex-col items-center justify-center gap-5">
+              <h1 className="text-4xl font-semibold">Great Job!</h1>
+              <h2 className="text-2xl font-semibold">Would you like to:</h2>
+              <div className="flex gap-4">
+                <button
+                  className="bg-laker-gold text-laker-purple w-35 p-2 rounded-2xl flex items-center justify-center gap-3 cursor-pointer hover:bg-[#E0A322] duration-300"
+                  onClick={() => setIndex(0)}
+                >
+                  <img src="/arrows/arrowPurple.svg" alt="Purple Arrow" />
+                  <h3>Start Over</h3>
+                </button>
+                <button
+                  className="bg-laker-purple text-white w-35 p-2 rounded-2xl flex items-center justify-center gap-3 cursor-pointer hover:bg-[#7831B7] duration-300"
+                  onClick={handleShuffle}
+                >
+                  <img src="/utility/shuffleIcon.svg" alt="Shuffle Icon" />
+                  <h3>Shuffle</h3>
+                </button>
+                <button
+                  className="text-white w-35 p-2 rounded-2xl flex items-center justify-center gap-3 cursor-pointer bg-red-400 text-white hover:bg-red-500 duration-300"
+                  onClick={() =>
+                    router.push(`/study/${setId}/${slugify(`${setName}`)}`)
+                  }
+                >
+                  <img
+                    src="/utility/hardIcon.svg"
+                    alt="X Icon"
+                    className="mt-0.5"
+                  />
+                  <h3>Back</h3>
+                </button>
+              </div>
+            </div>
+          </div>
+        )}
 
         <button
           className={`flex-shrink-0 h-full max-h-[500px] w-16 rounded-xl flex items-center justify-center transition-all duration-300 ${
-            index < cards.length - 1
+            index < cards.length
               ? "hover:bg-gray-300 cursor-pointer"
-              : "opacity-50 cursor-not-allowed"
+              : "invisible"
           }`}
           onClick={goToNext}
-          disabled={index === cards.length - 1}
+          disabled={index === cards.length}
         >
           <ChevronIcon className="h-8 w-8 select-none text-gray-700" />
         </button>
       </div>
 
-      {/* Difficulty Buttons */}
-      <div className="flex flex-col items-center justify-center gap-3">
+      {/* Bottom Buttons */}
+      <div
+        className={`flex flex-col items-center justify-center gap-3 ${
+          index >= cards.length && "invisible"
+        }`}
+      >
+        {/* Difficulty Buttons */}
         <div className="flex-shrink-0 flex items-center justify-center gap-5 h-16">
           <button
             className="bg-[#EF4444] text-white rounded-2xl px-6 py-3 w-48 text-lg cursor-pointer hover:bg-[#DC2626] transition-colors duration-300"
@@ -169,7 +219,7 @@ export const FlashcardLearn = ({
         </div>
         {/* Back Button */}
         <button
-          className=" flex justify-center items-center cursor-pointer hover:bg-gray-300 transition-all duration-300 rounded-xl p-1 w-48"
+          className="flex justify-center items-center cursor-pointer hover:bg-gray-300 rounded-xl p-1 w-48"
           onClick={() =>
             router.push(`/study/${setId}/${slugify(`${setName}`)}`)
           }
